43055f2460b1d7121701a9e5d8f648907a420130
rahmona-henry <rahmona.henry@icloud.com>
2019-08-14 01:02:15 +0100

    Add html file

--- server.js
+++ server.js
@@ -1,6 +1,24 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 const port = 3000
+
+app.use(express.static('client'))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+
+
+//POST Requests//
+app.post('https://api.hsforms.com/submissions/v3/integration/submit/6272961/5c48da10-a032-4aac-9a84-3e1ab646e066', (req, res) => {
+  console.log(req.body.firstname)
+  const firstname = req.body.firstname
+  const lastname = req.body.lastname
+  const email = req.body.email
+
+  res.send("response")
+})
 
 app.get('/', (req, res) => res.send('Hello Developer Advocate Team!'))
 app.listen(port, () => console.log(`Developer advocate app listening on ${port}!`))
