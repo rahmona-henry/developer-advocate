68dd145b9ffcc7c8699abb99f0530db8a68c34ad
rahmona-henry <rahmona.henry@icloud.com>
2019-08-14 17:30:18 +0100

    add cookie function

--- server.js
+++ server.js
@@ -1,24 +1,113 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// const xhr = new XMLHttpRequest();
 const port = 3000
+
 
 app.use(express.static('client'))
 
-app.use(bodyParser.json())
+
 app.use(bodyParser.urlencoded({ extended: true }))
-
-
+app.use(bodyParser.json())
+app.use(express.json({
+  inflate: true,
+  limit: '100kb',
+  reviver: null,
+  strict: true,
+  type: 'application/json',
+  verify: undefined
+}))
 
 //POST Requests//
-app.post('https://api.hsforms.com/submissions/v3/integration/submit/6272961/5c48da10-a032-4aac-9a84-3e1ab646e066', (req, res) => {
-  console.log(req.body.firstname)
+app.post('/', function (req, res) {
+
   const firstname = req.body.firstname
   const lastname = req.body.lastname
   const email = req.body.email
+  const cookie =req.body.cookie
 
-  res.send("response")
+  console.log(req.body)
+
+
+
+
+
+  // Create the new request
+  var xhr = new XMLHttpRequest();
+  var url = 'https://api.hsforms.com/submissions/v3/integration/submit/4014783/a4bb5614-3d17-4777-b76c-3e1b6d792202'
+
+  // Example request JSON:
+  var data = {
+    "submittedAt": "1565797980498",
+    "fields": [
+      {
+        "name": "email",
+        "value": email
+      },
+      {
+        "name": "firstname",
+        "value": firstname
+      }
+    ],
+    "context": {
+      "hutk":  cookie, // include this parameter and set it to the hubspotutk cookie value to enable cookie tracking on your submission
+      "pageUri": "www.example.com/page",
+      "pageName": "Example page"
+    },
+
+    "skipValidation": true
+  }
+
+  var final_data = JSON.stringify(data)
+
+  xhr.open('POST', url);
+  // Sets the value of the 'Content-Type' HTTP request headers to 'application/json'
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  //
+  xhr.onreadystatechange = function() {
+          console.log("RESPONSE: " + xhr.status);
+          console.log("RESPONSE MESSAGE: ");
+          if(xhr.readyState == 4 && xhr.status == 200) {
+              console.log(xhr.responseText); // Returns a 200 response if the submission is successful.
+          } else if (xhr.readyState == 4 && xhr.status == 400){
+              console.log(xhr.responseText); // Returns a 400 error the submission is rejected.
+          } else if (xhr.readyState == 4 && xhr.status == 403){
+              console.log(xhr.responseText); // Returns a 403 error if the portal isn't allowed to post submissions.
+          } else if (xhr.readyState == 4 && xhr.status == 404){
+              console.log(xhr.responseText); //Returns a 404 error if the formGuid isn't found
+          }
+         }
+
+
+  // Sends the request
+
+  xhr.send(final_data)
+
+
+
+  res.send('form submitted')
+
 })
 
-app.get('/', (req, res) => res.send('Hello Developer Advocate Team!'))
-app.listen(port, () => console.log(`Developer advocate app listening on ${port}!`))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/', (req, res) => res.send('Hello Dublin!'))
+app.listen(port, () => console.log(`Test app listening on ${port}!`))
